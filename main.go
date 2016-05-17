0a824205878124de496da01a1aa48eb8a90c5638
Yu-Ming Huang <rmx.z91@gmail.com>
2016-05-17 23:08:37 +0800

    Create simulation models.

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"github.com/rmxymh/infra-ecosphere/service"
+	"github.com/rmxymh/infra-ecosphere/model"
 )
 
 func main() {
+	// make default data
+	model.AddBMCUser("admin", "admin")
+
 	service.NetworkServiceRun()
 }
