5d96fe84e45cb5076809be6f856608261a4c0e3f
Yu-Ming Huang <rmx.z91@gmail.com>
2016-05-30 22:09:49 +0800

    Add implemented handler.

--- ipmi-proxy/main.go
+++ ipmi-proxy/main.go
@@ -9,6 +9,7 @@ func main() {
 	config := utils.LoadConfig("infra-ecosphere.cfg")
 	EcospherePort = config.WebAPIPort
 	ipmi.IPMI_CHASSIS_SET_BOOT_OPTION_SetHandler(ipmi.BOOT_FLAG, SetBootDevice)
+	ipmi.IPMI_CHASSIS_SetHandler(ipmi.IPMI_CMD_GET_CHASSIS_STATUS, HandleIPMIGetChassisStatus)
 	ipmi.IPMI_CHASSIS_SetHandler(ipmi.IPMI_CMD_CHASSIS_CONTROL, HandleIPMIChassisControl)
 	ipmi.IPMIServerServiceRun()
 }
